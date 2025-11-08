5b7e76e7dcc71a3e1ca69817c64b9a8a3f84e9fc
alfonsoce <alfonso.ceccarelli@gmail.com>
2025-11-08 21:36:53 +0100

    Delete bikerides-mvp-nextjs directory

--- bikerides-mvp-nextjs/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import BikeRidesPrototype from "./prototype/BikeRidesPrototype";
-
-export default function Page() { 
-  return <BikeRidesPrototype />; 
-}
