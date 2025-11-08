d66157e9abae841cc6a74e1fc57053766a7601db
alfonsoce <alfonso.ceccarelli@gmail.com>
2025-11-08 22:25:39 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import React from "react";
+import BikeRidesPrototype from "./prototype/BikeRidesPrototype";
+
 export default function Page() {
-  return <h1 style={{padding: 24, fontFamily: "system-ui"}}>BikeRides è online ✅</h1>;
+  return <BikeRidesPrototype />;
 }
