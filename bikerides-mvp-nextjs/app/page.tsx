13db28cdf92dfedb54a3c8d765b668ac5aa743c5
alfonsoce <alfonso.ceccarelli@gmail.com>
2025-11-08 21:04:18 +0100

    Add files via upload

---
+++ bikerides-mvp-nextjs/app/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import BikeRidesPrototype from "./prototype/BikeRidesPrototype";
+
+export default function Page() { 
+  return <BikeRidesPrototype />; 
+}
